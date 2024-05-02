0565e9b879686566cc06af9cd12b6c307a4301dd
Jean Luc Houedanou <jeanluc@bigfiveabidjan.com>
2024-05-02 10:49:32 +0000

    debbut css page pdt

--- page-41.php
+++ page-41.php
@@ -21,8 +21,10 @@ get_header();
 <div id='pagecontent'>
     <div class='contenudelapage'>
         <?php while (have_posts()) : the_post(); ?>
-            <h1><?php //the_title(); ?></h1>
-            <?php the_content(); ?>
+        <div class="crodie">
+
+        <?php the_content(); ?>
+        </div>
         <?php endwhile; ?>
     </div>
 </div>
