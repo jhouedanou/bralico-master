b42a4031690a4b1535e407a3f1f9fda5b7c4a0a9
Jean Luc Houedanou <jeanluc@bigfiveabidjan.com>
2024-05-02 10:21:00 +0000

    css page brasserie

--- footer.php
+++ footer.php
@@ -14,6 +14,26 @@
         <div class="row">
             <div id="vome" class="col-md-5">
                 <?php dynamic_sidebar('newsletter-footer');?>
+                <!-- menu reseaux sociaux -->
+                <?php
+							class Image_Walker_Nav_Menu2 extends Walker_Nav_Menu {
+								function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
+									$image_url = get_post_meta( $item->ID, '_menu_item_image_url', true );
+									$title = apply_filters( 'the_title', $item->title, $item->ID );
+									$url = $item->url;
+									$output .= "<li><a target='_blank' href='$url'><img src='$image_url' alt='$title' /></a></li>";
+								}
+							}
+
+							wp_nav_menu(array(
+								'menu' => 'Social Menu',
+								'container_id' => 'reseauxsociauxbralicofooter',
+								'menu_class' =>'reseauxsociauxbralicofooter',
+								'menu_id'=>'reseauxsociauxbralicofooter',
+								'theme_location' => 'Socialmenu',
+								'walker' => new Image_Walker_Nav_Menu2()
+							));
+						?>
             </div>
             <div class="col-md-4 solidifie">
                 <?php dynamic_sidebar('contacts-footer');?>
