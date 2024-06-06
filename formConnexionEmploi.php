443aead38a332895b6642a4e1d5bd2d3f0afca5a
Jean Luc Houedanou <jeanluc@houedanou.com>
2024-06-06 15:54:04 +0000

    s

--- formConnexionEmploi.php
+++ formConnexionEmploi.php
@@ -1,32 +1,18 @@
-           <div id="ilauncompte" class="col-md-6 col-xs-12">
-               <div class="gbox">
-                   <p class="mello">2</p>
-                   <h4>Vous avez déjà un compte ?</h4>
-                   <ul>
-                       <li>Connectez-vous pour mettre à jour votre CV et postuler aux offres.</li>
-                       <li>Gérez vos candidatures et suivez leur avancement.</li>
-                   </ul>
-                   <button id="open-login-modal" data-toggle="modal" data-target="#login-modal">Se connecter</button>
+<div id="ilauncompte" class="col-md-6 col-xs-12">
+    <div class="gbox">
+        <p class="mello">2</p>
+        <h4>Vous avez déjà un compte ?</h4>
+        <ul>
+            <li>Connectez-vous pour mettre à jour votre CV et postuler aux offres.</li>
+            <li>Gérez vos candidatures et suivez leur avancement.</li>
+        </ul>
 
-                   <div class="modal fade" id="login-modal" tabindex="-1" role="dialog"
-                       aria-labelledby="login-modal-label" aria-hidden="true">
-                       <div class="modal-dialog" role="document">
-                           <div class="modal-content">
-                               <div class="modal-header">
-                                   <h5 class="modal-title" id="login-modal-label">J'ai un compte</h5>
-                                   <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                       <span aria-hidden="true">&times;</span>
-                                   </button>
-                               </div>
-                               <div class="modal-body">
-                                   <?php
-                                                echo 'Veuillez vous connecter pour postuler aux offres d\'emploi.';
-                                                wp_login_form();
-                                            ?>
-                               </div>
-                           </div>
-                       </div>
-                   </div>
-               </div>
-
-           </div>
+        <button id="open-login-modal">Se connecter</button>
+        <div id="login-modal" title="J'ai un compte" style="display: none;">
+            <?php
+                echo 'Veuillez vous connecter pour postuler aux offres d\'emploi.';
+                wp_login_form();
+            ?>
+        </div>
+    </div>
+</div>
--- single-job_listing.php
+++ single-job_listing.php
@@ -9,82 +9,83 @@
 
 get_header();
 ?>
+
 <div id="pagecontent" class="container-fluid what">
     <div class="row">
         <div id="article" class="col-md-12">
-
-
             <main id="primary" class="site-main">
                 <div id="maincol" class="row">
                     <div class="col-md-6">
                         <div class="inseid">
-
                             <?php
-                                    $fields = array(
-                                        /* 'intitule_du_poste',
-                                        'departement',
-                                        'service',
-                                        'n°_du_poste',
-                                        'superieur_hierarchique',
-                                        'nombre_de_personnes_sous_sa_direction',
-                                         */'lieu',
-                                        'objectif_du_poste',
-                                        'responsabilites_principales',
-                                        'diplome_requis_pour_le_poste',
-                                        'specialite',
-                                        'competences_fonctionnelles',
-                                        'experience_professionnelle',
-                                        /* 'secteurdomaine',
-                                        'aptitudes',
-                                        'lieu_du_poste',
-                                         */'date_limite_de_reception_des_dossiers_'
-                                    );
+                            $fields = array(
+                                /* 'intitule_du_poste',
+                                'departement',
+                                'service',
+                                'n°_du_poste',
+                                'superieur_hierarchique',
+                                'nombre_de_personnes_sous_sa_direction',
+                                 */'lieu',
+                                'objectif_du_poste',
+                                'responsabilites_principales',
+                                'diplome_requis_pour_le_poste',
+                                'specialite',
+                                'competences_fonctionnelles',
+                                'experience_professionnelle',
+                                /* 'secteurdomaine',
+                                'aptitudes',
+                                'lieu_du_poste',
+                                 */'date_limite_de_reception_des_dossiers_'
+                            );
 
-                                    foreach ($fields as $field) {
-                                        $field_object = get_field_object($field);
-                                        $field_value = get_field($field);
-                                        if ($field_object && $field_value) : ?>
+                            foreach ($fields as $field) {
+                                $field_object = get_field_object($field);
+                                $field_value = get_field($field);
+                                if ($field_object && $field_value) :
+                            ?>
                             <div class="emploi-fields">
                                 <h5><?php echo $field_object['label']; ?></h5>
                                 <p><?php echo $field_value; ?></p>
                             </div>
-                            <?php endif;
-                                        }
-                                        ?>
+                            <?php
+                                endif;
+                            }
+                            ?>
                         </div>
-
                     </div>
                     <div class="col-md-6">
                         <div id="postsidebar">
                             <div class="content">
                                 <?php
-	while ( have_posts() ) :
-		the_post();
-
-		get_template_part( 'template-parts/content', get_post_type() );
-?>
+                                while (have_posts()) :
+                                    the_post();
+                                    get_template_part('template-parts/content', get_post_type());
+                                ?>
                                 <?php
-	/* 	the_post_navigation(
-			array(
-				'prev_text' => '<span class="nav-subtitle">' . esc_html__( '<', 'bralico' ) . '</span> <span class="nav-title">%title</span>',
-				'next_text' => '<span class="nav-subtitle">' .  '</span> <span class="nav-title">%title</span>'.esc_html__( ' >', 'bralico' ) ,
-			)
-		);
- */
-		// If comments are open or we have at least one comment, load up the comment template.
-		/* if ( comments_open() || get_comments_number() ) :
-			comments_template();
-		endif; */
+                                /* 	the_post_navigation(
+                                        array(
+                                            'prev_text' => '<span class="nav-subtitle">' . esc_html__( '<', 'bralico' ) . '</span> <span class="nav-title">%title</span>',
+                                            'next_text' => '<span class="nav-subtitle">' .  '</span> <span class="nav-title">%title</span>'.esc_html__( ' >', 'bralico' ) ,
+                                        )
+                                    );
+                                 */
+                                // If comments are open or we have at least one comment, load up the comment template.
+                                /* if ( comments_open() || get_comments_number() ) :
+                                    comments_template();
+                                endif; */
+                                endwhile; // End of the loop.
+                                ?>
+                                <div id="theacolyte">
 
-	endwhile; // End of the loop.
-	?>
-
+                                    <a id="bbenchwap" href="<?php echo get_permalink('304'); ?>">Mon espace
+                                        candidat</a>
+                                    <a href="<?php echo get_permalink('119');?>">Offres d'emploi</a>
+                                    <a href="' . wp_logout_url(home_url()) . '">Déconnexion</a>
+                                </div>
                             </div>
                         </div>
-
                     </div>
                 </div>
-
             </main><!-- #main -->
         </div>
     </div>
